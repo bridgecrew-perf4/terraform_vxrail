61c8bedf4d2c40c6c03ae6ff09908fce1e08aa4e
Stephen Akuffo <stephen_akuffo@se.isus.emc.com>
2021-03-30 19:50:51 -0400

    demo done

--- tf_examples/vxrail_tf/main.tf
+++ tf_examples/vxrail_tf/main.tf
@@ -8,7 +8,8 @@ provider "vsphere" {
 }
 
 resource "vsphere_virtual_machine" "learn" {
-  name             = "tf-on-vxrail"
+  count = 5
+  name             = "tf-on-vxrail-${count.index}"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
